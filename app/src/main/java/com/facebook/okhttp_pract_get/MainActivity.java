37d15687083db8ed928200cd7be9ba6c25673011
Uzair Majeed <uzairmajeed@uzairmajeeds-MacBook-Pro.local>
2023-08-31 11:16:19 +0530

    FirstCommit

---
+++ app/src/main/java/com/facebook/okhttp_pract_get/MainActivity.java
@@ -0,0 +1,109 @@
+package com.facebook.okhttp_pract_get;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+TextView textView;
+Button buttonget,buttonpost;
+    OkHttpClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textview);
+        buttonget=findViewById(R.id.getbutton);
+        buttonpost=findViewById(R.id.postbutton);
+
+        buttonget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getfunction();
+            }
+        });
+
+        buttonpost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postfunction();
+            }
+        });
+    }
+
+    private void postfunction() {
+        client=new OkHttpClient();
+        RequestBody requestBody=new FormBody.Builder()
+                .add("Name","uzair")
+                .build();
+
+      Request request=new Request.Builder()
+              .url("https://reqres.in/api/users")
+              .post(requestBody)
+              .build();
+      client.newCall(request).enqueue(new Callback() {
+          @Override
+          public void onFailure(@NonNull Call call, @NonNull IOException e) {
+              e.printStackTrace();
+          }
+
+          @Override
+          public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+              if (response.isSuccessful()){
+                  String myrequest=response.body().string();
+                  MainActivity.this.runOnUiThread(new Runnable() {
+                      @Override
+                      public void run() {
+                          textView.setText(myrequest);
+                      }
+                  });
+              }
+
+          }
+      });
+    }
+    private void getfunction(){
+        client=new OkHttpClient();
+        String url="https://dummyjson.com/products/5";
+
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String myresponse=response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(myresponse);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
